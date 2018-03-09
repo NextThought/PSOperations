9ef5503366e80de8cca9c82e219fdcfc276b37f8
Bryan Hoke <bryan.hoke@nextthought.com>
2018-03-09 16:31:17 -0600

    Update for Swift 4.0.

--- PSOperations/Operation.swift
+++ PSOperations/Operation.swift
@@ -121,7 +121,7 @@ open class Operation: Foundation.Operation {
             */
             willChangeValue(forKey: "state")
             
-            stateLock.withCriticalScope { Void -> Void in
+            stateLock.withCriticalScope { () -> Void in
                 guard _state != .finished else {
                     return
                 }
